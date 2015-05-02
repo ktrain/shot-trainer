3383e098278f959dc9f9331fc9bd98f4d602992f
Jared Tyler <jared.tyler@thalmic.com>
2015-05-02 02:44:44 -0400

    Moving debug screen to an actual Debug Activity and cleaning it up a bit

--- app/src/main/java/com/intjltd/shottrainer/DebugActivity.java
+++ app/src/main/java/com/intjltd/shottrainer/DebugActivity.java
@@ -1,28 +1,29 @@
 package com.intjltd.shottrainer;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.thalmic.myo.AbstractDeviceListener;
-import com.thalmic.myo.Arm;
 import com.thalmic.myo.DeviceListener;
 import com.thalmic.myo.Hub;
 import com.thalmic.myo.Myo;
-import com.thalmic.myo.Pose;
 import com.thalmic.myo.Quaternion;
 import com.thalmic.myo.Vector3;
-import com.thalmic.myo.XDirection;
 import com.thalmic.myo.internal.util.ByteUtil;
 import com.thalmic.myo.scanner.ScanActivity;
 
 
-public class MainActivity extends ActionBarActivity {
+public class DebugActivity extends ActionBarActivity {
+
+    private EditText txtOrientation;
+    private EditText txtAccelerometer;
+    private EditText txtGyroscope;
+    private EditText txtEmgData;
 
     private DeviceListener myoListener = new AbstractDeviceListener() {
         @Override
@@ -32,35 +33,72 @@ public class MainActivity extends ActionBarActivity {
 
         @Override
         public void onOrientationData(Myo myo, long timestamp, Quaternion rotation) {
-
+            txtOrientation.setText(
+                "x: " + rotation.x() + "\n" +
+                "y: " + rotation.y() + "\n" +
+                "z: " + rotation.z() + "\n" +
+                "w: " + rotation.w()
+            );
         }
 
         @Override
         public void onAccelerometerData(Myo myo, long timestamp, Vector3 accel) {
-
+            txtAccelerometer.setText(
+                "x: " + accel.x() + "\n" +
+                "y: " + accel.y() + "\n" +
+                "z: " + accel.z()
+            );
         }
 
         @Override
         public void onGyroscopeData(Myo myo, long timestamp, Vector3 gyro) {
-
+            txtGyroscope.setText(
+                "x: " + gyro.x() + "\n" +
+                "y: " + gyro.y() + "\n" +
+                "z: " + gyro.z()
+            );
         }
 
         @Override
         public void onEmgData(Myo myo, long timestamp, byte[] emg) {
-
+            txtEmgData.setText(ByteUtil.bytesToHex(emg));
         }
     };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_debug);
+
+        txtOrientation = (EditText) findViewById(R.id.txtOrientation);
+        txtAccelerometer = (EditText) findViewById(R.id.txtAccelerometer);
+        txtGyroscope = (EditText) findViewById(R.id.txtGyroscope);
+        txtEmgData = (EditText) findViewById(R.id.txtEmgData);
+
+        Hub hub = Hub.getInstance();
+        if (!hub.init(this, getPackageName())) {
+            Toast.makeText(this, "Couldn't initialize Hub", Toast.LENGTH_SHORT).show();
+            finish();
+            return;
+        }
+
+        hub.addListener(myoListener);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Hub.getInstance().removeListener(myoListener);
+
+        if (isFinishing()) {
+            Hub.getInstance().shutdown();
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_debug, menu);
         return true;
     }
 
@@ -76,22 +114,12 @@ public class MainActivity extends ActionBarActivity {
             return true;
         }
 
-        if (id == R.id.action_debug) {
-            onDebugActionSelected();
-            return true;
-        }
-
         if (id == R.id.action_myoScan) {
             onScanActionSelected();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    protected void onDebugActionSelected() {
-        Intent intent = new Intent(this, DebugActivity.class);
-        startActivity(intent);
     }
 
     protected void onScanActionSelected() {
